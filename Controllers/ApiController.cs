90b4eb1acd4c8576ecacc815d244ce9d6202ba87
Ian Fay <ianfay2003@gmail.com>
2023-09-13 09:39:37 -0500

    Work Completed in Class

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Nugatory.Models;
+
+namespace WordApi.Controllers
+{
+    [ApiController, Route("[controller]/word")]
+    public class ApiController : ControllerBase
+    {
+        private DataContext _dataContext;
+
+        public ApiController(DataContext db)
+        {
+            _dataContext = db;
+        }
+        // http get entire collection
+        [HttpGet, SwaggerOperation(summary: "returns all words", null)]
+        public IEnumerable<WordColor> Get()
+        {
+            return _dataContext.WordColors;
+        }
+        // http get specific member of collection
+        [HttpGet("{id}"), SwaggerOperation(summary: "returns specific word", null)]
+        public WordColor Get(int id)
+        {
+            return _dataContext.WordColors.Find(id);
+        }
+        // http post member to collection
+        [HttpPost, SwaggerOperation(summary: "add word to collection", null), ProducesResponseType(typeof(WordColor), 201), SwaggerResponse(201, "Created")]
+        public async Task<ActionResult<WordColor>> Post([FromBody] WordColor wordColor) {
+            _dataContext.Add(wordColor);
+            await _dataContext.SaveChangesAsync();
+            return wordColor;
+        }
+        // http delete member from collection
+        [HttpDelete("{id}"), SwaggerOperation(summary: "delete word from collection", null), ProducesResponseType(typeof(WordColor), 204), SwaggerResponse(204, "No Content")]
+        public async Task<ActionResult> Delete(int id){
+            WordColor wordColor = await _dataContext.WordColors.FindAsync(id);
+            if (wordColor == null){
+                return NotFound();
+            }
+            _dataContext.Remove(wordColor);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Nugatory.Models;
+
+// Connection info stored in appsettings.json
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Register the DataContext service
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(configuration["ConnectionStrings:DefaultSQLiteConnection"]));
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Open",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "Nugatory", 
+        Version = "v1",
+        Description = "A totally useless API",
+        });
+    c.EnableAnnotations();
+    c.TagActionsBy(api => new[] { api.HttpMethod });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+// }
+
+app.UseCors("Open");
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
